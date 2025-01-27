5f74387dfa407cd300e75613d4fa0a067926576b
olga-sdev <olga.github@airpost.net>
2025-01-27 20:18:34 +0200

    Create intro_pep.py

---
+++ Intro-PEP/intro_pep.py
@@ -0,0 +1,20 @@
+"""
+PEP is the language standards that provides information about many changes and processes related to Python.
+
+Python Enhancement Proposals -> is a collection of guidelines, best practices, descriptions of (new) features and
+implementations, as well as processes, mechanisms and important information surrounding Python.
+
+Types of PEPs:
+
+Standards Track PEPs -> describes new language features and implementations;
+Informational PEPs -> describes Python design issues and provide guidelines and information to Python community;
+Process PEPs -> describe various processes that revolve around Python
+(e.g., propose changes, provide recommendations, specify certain procedures).
+
+PEP 1 defines:
+
+Python’s Steering Council, i.e., a five-person committee and the final authorities who accept or reject PEPs;
+Python’s Core Developers, i.e., the group of volunteers who manage Python, and;
+Python’s BDFL, i.e., Guido van Rossum, the original creator of Python, who served as the project’s Benevolent Dictator For Life until 2018, when he resigned from the decision-making process.
+
+"""
