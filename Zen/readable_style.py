68eff128cff514a5e3befdb5149dd830b2db8f76
olga-sdev <olga.github@airpost.net>
2025-01-28 02:20:44 +0200

    Create readable_style.py

---
+++ Zen/readable_style.py
@@ -0,0 +1,13 @@
+"""
+<Beautiful is better than ugly>
+79-character maximum line length, variable naming conventions, placing statements on separate lines.
+
+"""
+
+import datetime
+
+today = datetime.date.today()
+
+print(today)
+
+# 2025-01-28
