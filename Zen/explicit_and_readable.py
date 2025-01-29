8660bb1f4cce1ce1f4b7cf19bf59a22077985590
olga-sdev <olga.github@airpost.net>
2025-01-29 22:28:28 +0200

    Create explicit_and_readable.py

---
+++ Zen/explicit_and_readable.py
@@ -0,0 +1,11 @@
+"""
+In Python, it’s preferred to use not only the simplest way to express a programming idea,
+but also the most explicit, concrete, specific one.
+
+Istead => from datetime import *
+Do => from datetime import datetime
+"""
+
+from datetime import datetime
+
+now = datetime.now()
