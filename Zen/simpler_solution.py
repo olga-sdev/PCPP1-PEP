ada1411dba3d40a038a7f26fff37fbaecc7e1e50
olga-sdev <olga.github@airpost.net>
2025-01-30 06:52:00 +0200

    Create simpler_solution.py

---
+++ Zen/simpler_solution.py
@@ -0,0 +1,8 @@
+"""
+Simpler solution is usually preferred over a complex one.
+Divide problems into smaller, simpler parts.
+"""
+
+months = ['march', 'april', 'may']
+
+[print(month.title()) for month in months]
