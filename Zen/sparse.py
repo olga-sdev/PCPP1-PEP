d9a049b9f47101577d29e80a3ba2cd0ba89d237b
olga-sdev <olga.github@airpost.net>
2025-02-02 17:01:34 +0200

    Create sparse.py

---
+++ Zen/sparse.py
@@ -0,0 +1,12 @@
+"""
+Reduce density -> balance between nesting and sparcsity
+For better readability
+"""
+
+status = 'Off'
+
+if status == 'On':
+  print('Laptop is On')
+else:
+  print('Laptop is Off')
+      
