90efab2c78e32ef8f7db83b2ff57f6805024c878
olga-sdev <olga.github@airpost.net>
2025-02-05 23:02:29 +0200

    Create exception_to_rules.py

---
+++ Zen/exception_to_rules.py
@@ -0,0 +1,8 @@
+"""
+Keep code styling convention and functionality balance.
+Make exceptions to rules in case:
+* better performance and maintainability;
+* long line of code because splitting it into two separate lines affects readability;
+* compatibility with previously written code: use CamelCase instead of snake_case.
+Rules might be broken, exceptions might be made.
+"""
