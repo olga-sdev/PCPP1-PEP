798fa32f5cbd7855e582d13a35dab826b57eaa90
olga-sdev <olga.github@airpost.net>
2025-02-07 21:56:49 +0200

    Create obvious_way.py

---
+++ Zen/obvious_way.py
@@ -0,0 +1,5 @@
+"""
+Multiple ways for implementation? -> Agree on the best way to achieve a particular goal.
+Follow the language use standards and conventions.
+Each entity (function / class / method) –> Single cohesive responsibility.
+"""
