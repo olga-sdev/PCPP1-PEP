cc9ee40c0dbea7168099c2870ee7b9ded08db77a
olga-sdev <olga.github@airpost.net>
2025-02-08 18:06:28 +0200

    Create good_implementation.py

---
+++ Zen/good_implementation.py
@@ -0,0 +1,5 @@
+"""
+implementation is easy to explain -> good idea:
+Everything that can be explained in words -> can be translated into code and turned into a well-operating computer program.
+Keep things simple; the simpler, the better.
+"""
