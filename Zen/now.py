a2093419c9eaccb2d86c2849a6eb51e1b945796e
olga-sdev <olga.github@airpost.net>
2025-02-08 18:04:19 +0200

    Create now.py

---
+++ Zen/now.py
@@ -0,0 +1,8 @@
+"""
+There is no perfect thing. "You can work hard to move closer to perfection, refine your code, refactor it several times, but it will never be perfect."
+Perfect is the enemy of good - faster is the enemy of slower.
+Function’s not working as expected and it's not possible to fix it today -> Mark it as deprecated so that you don’t forget about it ->
+"""
+
+def deprecated_functions():
+    raise DeprecationWarning
