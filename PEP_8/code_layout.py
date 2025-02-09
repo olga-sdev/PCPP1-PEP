67df6b6b1631774019f7c22b1657ed2ca08004e4
olga-sdev <olga.github@airpost.net>
2025-02-09 19:01:37 +0200

    Create code_layout.py

---
+++ PEP_8/code_layout.py
@@ -0,0 +1,11 @@
+"""
+Style recommendations:
+
+* indentation with tabs and spaces (4 spaces per indentation level -> spaces rather than tabs);
+* line length | breaks | blank lines;
+* source file encoding and module imports.
+"""
+
+
+def example_with_spaces():
+    return '4 spaces before return'
