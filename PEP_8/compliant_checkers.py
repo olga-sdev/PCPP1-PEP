a37917aba200b8a5137d93ca484b7a891c1cf415
olga-sdev <olga.github@airpost.net>
2025-02-09 18:56:41 +0200

    Create compliant_checkers.py

---
+++ PEP_8/compliant_checkers.py
@@ -0,0 +1,8 @@
+"""
+pycodestyle -> Python style guide checker.
+Check style conventions in PEP 8.
+
+autopep8 -> automatically format Python code to conform to the PEP 8 guidelines.
+"""
+
+$ pip install pycodestyle
