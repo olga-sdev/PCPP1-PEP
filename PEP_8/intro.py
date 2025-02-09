218eefe9f0955fae0fc248a0d14f7803dd0d4a03
olga-sdev <olga.github@airpost.net>
2025-02-09 18:52:49 +0200

    Create intro.py

---
+++ PEP_8/intro.py
@@ -0,0 +1,10 @@
+"""
+The code will be read much more often than it will be written.
+PEP 8 is about additional conventions.
+
+Cases to ignore some specific PEP 8 guidelines, if following them:
+
+* break backwards compatibility.
+* negative effect on readability.
+* cause inconsistency with the rest of the code.
+"""
