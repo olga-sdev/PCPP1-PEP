95aaba4244793452629a5df2ae53bdfe9f40526d
olga-sdev <olga.github@airpost.net>
2025-02-10 20:55:22 +0200

    Create continuation_lines.py

---
+++ PEP_8/continuation_lines.py
@@ -0,0 +1,30 @@
+"""
+Continuation lines -> logical lines of code to split cause of length or for improving readability: with a help of parentheses/brackets/braces
+Info https://www.python.org/dev/peps/pep-0008/#indentation.
+
+Limit line to a max of 79 chars.
+
+Limin line to max of 72 chars for comments/docstrings.
+
+Break line before binary operator.
+"""
+
+lines_list_1 = [
+    line_1,
+    line_2
+    ]
+
+lines_list_2 = [
+    line_1,
+    line_2
+]
+
+def function_lines(
+        line_1,
+        line_2):
+    return line_1, line_2
+
+
+lines = (line_1
+        + line_2
+        + line_3)
