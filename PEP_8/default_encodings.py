64694181428bd61ea52f2acde0fac1486784d596
olga-sdev <olga.github@airpost.net>
2025-02-10 21:55:30 +0200

    Create default_encodings.py

---
+++ PEP_8/default_encodings.py
@@ -0,0 +1,20 @@
+"""
+Use Python’s default encodings -> Python 3 -- UTF-8, Python 2 -- ASCII.
+
+imports -> on separate lines.
+
+Use absolute imports.
+
+imports at the beginning of the script.
+
+Avoid using wildcard imports: `from module import *`
+"""
+
+# Example of import on separate line
+import pandas as pd
+
+# Example of from … import … syntax
+from selenium import webdriver
+
+# Example with absolute imports
+from config.settings import Config
