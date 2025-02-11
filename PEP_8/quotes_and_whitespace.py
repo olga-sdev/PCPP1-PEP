f12269f85ba09765c8313c6d60dec17bd5dada5e
olga-sdev <olga.github@airpost.net>
2025-02-11 18:04:57 +0200

    Create quotes_and_whitespace.py

---
+++ PEP_8/quotes_and_whitespace.py
@@ -0,0 +1,11 @@
+"""
+In Python there types of quots:
+* single-quoted (e.g., 'quotes');
+* double-quoted (e.g., "quotes") strings.
+
+Avoid too much whitespace:
+* avoid excessive whitespace immediately inside parentheses/brackets/braces;
+* or immediately before a comma/semicolon/colon
+"""
+
+quoted_set = {'set item 1', ['item 2'], {'key': 'value'}}
