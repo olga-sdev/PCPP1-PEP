f384848116836dc5346fe7b03615be37e0f73e7d
olga-sdev <olga.github@airpost.net>
2025-02-12 01:25:09 +0200

    Create naming_conventions.py

---
+++ PEP_8/naming_conventions.py
@@ -0,0 +1,15 @@
+"""
+Naming styles:
+* CamelCase
+* mixedCase
+* _single_leading_underscore
+* single_trailing_underscore_ => avoid any conflicts with Python keywords, e.g., class_
+* __double_leading_underscore -> invokes name mangling
+__magic_objects__ -> __init__
+
+RecommendationsL
+* variable, function, method, module,  ->lowercase letter or word(s), and separate words by underscores;
+* class, exception -> CamelCase;
+* NEW_CONSTANT -> uppercase letters and separate words by underscores;
+* package -> lowercase nonseparated word.
+"""
