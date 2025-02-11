934474ae02983b2be7b2fd98841cef3a52e65642
olga-sdev <olga.github@airpost.net>
2025-02-12 00:56:06 +0200

    Update quotes_and_whitespace.py

--- PEP_8/quotes_and_whitespace.py
+++ PEP_8/quotes_and_whitespace.py
@@ -8,7 +8,9 @@ Avoid too much whitespace:
 * immediately before a comma/semicolon/colon;
 * after a trailing comma followed by a closing parenthesis;
 * immediately before an opening parenthesis that marks the beginning of the argument list of a function invocation;
-* immediately before an opening parenthesis that marks the beginning of indexing/slicing.
+* immediately before an opening parenthesis that marks the beginning of indexing/slicing;
+* no more then one space before and after operators => variable = 2 + 2
+* surround the = operator with spaces if it’s used to indicate a keyword argument/default value => def function(y=2)
 """
 
 quoted_set = {'set item 1', ['item 2'], {'key': 'value'}}
