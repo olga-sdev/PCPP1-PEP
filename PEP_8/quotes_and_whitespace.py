ed49775086ca350e777a91da0b19033ee3dd17b6
olga-sdev <olga.github@airpost.net>
2025-02-12 00:51:46 +0200

    Update quotes_and_whitespace.py

--- PEP_8/quotes_and_whitespace.py
+++ PEP_8/quotes_and_whitespace.py
@@ -5,7 +5,10 @@ In Python there types of quots:
 
 Avoid too much whitespace:
 * avoid excessive whitespace immediately inside parentheses/brackets/braces;
-* or immediately before a comma/semicolon/colon
+* immediately before a comma/semicolon/colon;
+* after a trailing comma followed by a closing parenthesis;
+* immediately before an opening parenthesis that marks the beginning of the argument list of a function invocation;
+* immediately before an opening parenthesis that marks the beginning of indexing/slicing.
 """
 
 quoted_set = {'set item 1', ['item 2'], {'key': 'value'}}
