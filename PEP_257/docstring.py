4f6c02b4067e5094ff11bf8fdf41c2c25b6578d5
olga-sdev <olga.github@airpost.net>
2025-02-12 01:42:48 +0200

    Create docstring.py

---
+++ PEP_257/docstring.py
@@ -0,0 +1,6 @@
+"""
+PEP 257 -> standardize the high-level structure of docstrings.
+
+Docstring -> string literal in first statement in a module, function, class, or method definition (describing functionality and capabilities)
+Dostring becomes the __doc__ special attribute of the object.
+"""
