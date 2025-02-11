4c3955f93fa496446dd62614d2387f88960dd825
olga-sdev <olga.github@airpost.net>
2025-02-12 01:32:23 +0200

    Create programming_recommendations.py

---
+++ PEP_8/programming_recommendations.py
@@ -0,0 +1,16 @@
+# comparisons to the None -> with is or is not:
+if var is not None:
+    pass
+
+
+# comparisons to the Booleans -> with is or is not:
+if var is True:
+  pass
+
+if var:
+  pass
+
+
+# checking for prefixes or suffixes -> ''.startswith() and ''.endswith() string methods:
+if var.startswith('Life'):
+    pass
