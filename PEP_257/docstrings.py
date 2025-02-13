1b3ab3b00095cbd946a6b470bb00409cf5be394f
olga-sdev <olga.github@airpost.net>
2025-02-14 00:27:29 +0200

    Update docstrings.py

--- PEP_257/docstrings.py
+++ PEP_257/docstrings.py
@@ -4,11 +4,22 @@ All public modules, functions, classes, and methods -> add docstrings.
 Attribute docstrings -> after an assignment statement at the top level of a module, class, or the __init__ method;
 Additional dosctrings, which are located immediately after another docstring.
 
-One-line docstrings -> short descriptions;
-Multi-line docstrings –> more elaborate description.
+One-line docstrings -> short descriptions; prescribe the code segment's effect ("Make smth"); 
+Multi-line docstrings –> more elaborate description; document the function, command line syntax, env variables, files.
 """
 
 
 def docsting_function():
-  """Docstring example"""
+  """Create Docstring example"""
   pass
+
+
+def docsting_multi_function():
+  """Docstring exmple
+  
+  Attributes:
+  ...
+
+  Methods:
+  ...
+  """
