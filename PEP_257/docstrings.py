ec3b62d7bbbbe78fe156bb53069d21cdff992e1a
olga-sdev <olga.github@airpost.net>
2025-02-14 00:19:31 +0200

    Create docstrings.py

---
+++ PEP_257/docstrings.py
@@ -0,0 +1,14 @@
+"""
+All public modules, functions, classes, and methods -> add docstrings.
+
+Attribute docstrings -> after an assignment statement at the top level of a module, class, or the __init__ method;
+Additional dosctrings, which are located immediately after another docstring.
+
+One-line docstrings -> short descriptions;
+Multi-line docstrings –> more elaborate description.
+"""
+
+
+def docsting_function():
+  """Docstring example"""
+  pass
