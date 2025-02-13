dde40fd7e0e8821ed7a953f077acc1a40391f6c1
olga-sdev <olga.github@airpost.net>
2025-02-14 00:37:00 +0200

    Create linters_and_fixers.py

---
+++ PEP_257/linters_and_fixers.py
@@ -0,0 +1,24 @@
+"""
+Linter -> tool that analyzes stylistic anomalies and programming errors against a set of pre-defined rules. 
+It reports issues:
+* structural errors;
+* syntax errors;
+* consistency breakups;
+* lack of compatibility with best practices;
+* code style guidelines such as PEP 8.
+
+Popular linters are: 
+* Flake8, 
+* Pylint, 
+* Pyflakes, 
+* Pychecker, 
+* Mypy, 
+* Pycodestyle (formerly Pep8).
+
+Fixer -> program that fix issues and format your code to be consistent with the adopted standards.
+
+Popular fixers are: 
+* Black, 
+* YAPF, 
+* autopep8.
+"""
