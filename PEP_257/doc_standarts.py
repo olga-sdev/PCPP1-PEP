14f1f441b37c5947f062d2cdbc92957e0a2b2346
olga-sdev <olga.github@airpost.net>
2025-02-14 00:31:40 +0200

    Create doc_standarts.py

---
+++ PEP_257/doc_standarts.py
@@ -0,0 +1,8 @@
+"""
+Generally, a project consists doc elements:
+
+* readme -> summary of the project, its purpose, and possibly some installation guidelines;
+* examples.py -> script that demonstrates a few examples of how to utilize the project;
+* license in the form of a txt file (particularly important for Open Source and Public Domain projects)
+* how to contribute file -> information about the possible ways of contributing to the project (shared, open source, and public domain projects).
+"""
