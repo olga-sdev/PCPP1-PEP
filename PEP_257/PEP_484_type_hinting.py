ae4632ee61e5d8705f885fec95f132c821315342
olga-sdev <olga.github@airpost.net>
2025-02-14 00:08:52 +0200

    Create PEP_484_type_hinting.py

---
+++ PEP_257/PEP_484_type_hinting.py
@@ -0,0 +1,11 @@
+"""
+Type hiting -> additional information without comments, statically indicate the type information related to Python objects.
+
+It reduces the risk of passing a wrong type of value to the function.
+
+Type hinting is optional.
+"""
+
+
+def show_time(day: int) -> int:
+    return day
