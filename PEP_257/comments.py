98b67ca99042f750844b378e09fb8d1ed50a31d0
olga-sdev <olga.github@airpost.net>
2025-02-13 23:00:46 +0200

    Create comments.py

---
+++ PEP_257/comments.py
@@ -0,0 +1,15 @@
+"""
+Comment purposes:
+* tag;
+* code to verify;
+* plan
+"""
+
+# Commetns-tag of code for further improvement. Example:
+# TODO: Update according feature JIRA-1313
+
+# Comment code for verification. Example:
+
+# Make a plan. Example:
+# Step 1: ...
+# Step 2: ...
